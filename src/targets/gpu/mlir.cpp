f850cfc42bf4aef43dba6ae21bb7bf914b1fb993
Paul <pfultz2@yahoo.com>
2021-11-09 17:47:13 -0600

    Move mlir to the gpu and update the test

--- src/targets/gpu/mlir.cpp
+++ src/targets/gpu/mlir.cpp
@@ -1,48 +1,19 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2015-2022 Advanced Micro Devices, Inc. All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 #include <migraphx/gpu/mlir.hpp>
 
-#ifdef MIGRAPHX_MLIR
 #include <mlir-c/IR.h>
 #include <mlir-c/BuiltinAttributes.h>
 #include <mlir-c/BuiltinTypes.h>
 #include <mlir-c/Diagnostics.h>
+#include <mlir-c/Dialect/Standard.h>
 #include <mlir-c/Dialect/MIGraphX.h>
 #include <mlir-c/IntegerSet.h>
-#include <mlir-c/Pass.h>
 #include <mlir-c/Registration.h>
-#endif
 
-#include <migraphx/env.hpp>
 #include <migraphx/manage_ptr.hpp>
 #include <migraphx/module.hpp>
 #include <migraphx/instruction.hpp>
 #include <migraphx/config.hpp>
 #include <migraphx/ranges.hpp>
-#include <migraphx/gpu/code_object_op.hpp>
-#include <migraphx/gpu/context.hpp>
-#include <migraphx/gpu/device_name.hpp>
 #include <migraphx/iterator_for.hpp>
 #include <deque>
 #include <variant>
@@ -51,9 +22,6 @@ namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 namespace gpu {
 
-MIGRAPHX_DECLARE_ENV_VAR(MIGRAPHX_TRACE_MLIR);
-
-#ifdef MIGRAPHX_MLIR
 template <class T, class F, F f> // NOLINT
 struct mlir_handle
 {
@@ -111,7 +79,6 @@ using mlir_op_printing_flags = MIGRAPHX_MANAGE_MLIR_HANDLE(MlirOpPrintingFlags,
                                                            mlirOpPrintingFlagsDestroy);
 using mlir_region            = MIGRAPHX_MANAGE_MLIR_HANDLE(MlirRegion, mlirRegionDestroy);
 using mlir_block             = MIGRAPHX_MANAGE_MLIR_HANDLE(MlirBlock, mlirBlockDestroy);
-using mlir_pass_manager      = MIGRAPHX_MANAGE_MLIR_HANDLE(MlirPassManager, mlirPassManagerDestroy);
 
 std::string_view to_string_view(MlirStringRef s) { return {s.data, s.length}; }
 
@@ -123,12 +90,9 @@ MlirStringRef make_mlir_string_ref(const std::string_view& s)
 template <class F, class T, class Printer>
 void mlir_print(F f, T x, Printer printer)
 {
-    f(
-        x,
-        +[](MlirStringRef s, void* data) {
-            (*reinterpret_cast<Printer*>(data))(to_string_view(s));
-        },
-        &printer);
+    f(x,
+      +[](MlirStringRef s, void* data) { (*reinterpret_cast<Printer*>(data))(to_string_view(s)); },
+      &printer);
 }
 
 template <class F, class T>
@@ -152,8 +116,6 @@ struct mlir_program
           location(mlirLocationUnknownGet(ctx.get())),
           mmodule(mlirModuleCreateEmpty(location))
     {
-        MlirDialectHandle mixr_handle = mlirGetDialectHandle__migraphx__();
-        mlirDialectHandleRegisterDialect(mixr_handle, ctx.get());
         mlirRegisterAllDialects(ctx.get());
         mlirContextSetAllowUnregisteredDialects(ctx.get(), true /*allow*/);
     }
@@ -213,17 +175,10 @@ struct mlir_program
 
     MlirAttribute attribute(std::int64_t i) const
     {
-        if(i < 0)
-            MIGRAPHX_THROW("MLIR cant handle negative values since they are ambiguous");
-        return mlirIntegerAttrGet(mlirIntegerTypeGet(ctx.get(), 64), i);
+        return mlirIntegerAttrGet(mlirIntegerTypeSignedGet(ctx.get(), 64), i);
     }
-    MlirAttribute attribute(std::uint64_t i) const
-    {
-        if(i > (std::numeric_limits<std::uint64_t>::max() / 2))
-            MIGRAPHX_THROW("MLIR cant handle large integer values since they are ambiguous");
-        return mlirIntegerAttrGet(mlirIntegerTypeGet(ctx.get(), 64), i);
-    }
-    MlirAttribute attribute(unsigned char i) const { return attribute(std::uint64_t(i)); }
+    MlirAttribute attribute(std::uint64_t i) const { return attribute(std::int64_t(i)); }
+    MlirAttribute attribute(unsigned char i) const { return attribute(std::int64_t(i)); }
     MlirAttribute attribute(bool b) const { return mlirBoolAttrGet(ctx.get(), b ? 1 : 0); }
     MlirAttribute attribute(double d) const
     {
@@ -400,7 +355,7 @@ struct mlir_program
         std::vector<MlirValue> result;
         mlir_operation op = ops.create_operation();
         auto weak_op      = op.get();
-        mlirBlockAppendOwnedOperation(body, op.release());
+        mlirBlockInsertOwnedOperation(body, 0, op.release());
 
         auto n = mlirOperationGetNumResults(weak_op);
         result.reserve(n);
@@ -414,7 +369,6 @@ struct mlir_program
     insert(MlirBlock body, const module& m, std::unordered_map<instruction_ref, MlirValue>& ins_map)
     {
         auto names = m.get_parameter_names();
-        std::sort(names.begin(), names.end());
         std::vector<shape> inputs;
         std::transform(names.begin(),
                        names.end(),
@@ -422,55 +376,21 @@ struct mlir_program
                        [&](const std::string& name) { return m.get_parameter_shape(name); });
         std::vector<shape> outputs = m.get_output_shapes();
 
-        std::vector<MlirLocation> arg_locs(inputs.size(), location);
         auto body_inputs   = make_tensors(inputs);
         mlir_region region = mlirRegionCreate();
-        mlir_block fbody = mlirBlockCreate(body_inputs.size(), body_inputs.data(), arg_locs.data());
-        MlirBlock result = fbody.get();
+        mlir_block fbody   = mlirBlockCreate(body_inputs.size(), body_inputs.data());
+        MlirBlock result   = fbody.get();
         mlirRegionAppendOwnedBlock(region.get(), fbody.release());
 
-        auto ops = create_operation_state("func.func");
-        ops.add_attributes({{"function_type", make_function_type(inputs, outputs)},
-                            {"sym_name", std::string("main")},
-                            {"kernel", std::string("mixr")}});
+        auto ops = create_operation_state("builtin.func");
+        ops.add_attributes(
+            {{"type", make_function_type(inputs, outputs)}, {"sym_name", std::string("main")}});
         ops.add_region(std::move(region));
         insert(body, std::move(ops));
 
         for(auto i : range(names.size()))
             ins_map[m.get_parameter(names[i])] = mlirBlockGetArgument(result, i);
         return result;
-    }
-
-    static std::string get_name(instruction_ref ins)
-    {
-        if(ins->name() == "@return")
-            return "func.return";
-        return "migraphx." + ins->name();
-    }
-
-    static value get_operator_value(const operation& op)
-    {
-        auto v = op.to_value();
-        if(op.name() == "convolution")
-        {
-            // Adjust symetrical padding
-            if(v.at("padding").size() == v.at("stride").size())
-            {
-                auto padding = v.at("padding");
-                std::copy(padding.begin(), padding.end(), std::back_inserter(v.at("padding")));
-            }
-        }
-        return v;
-    }
-
-    static shape get_shape(instruction_ref ins)
-    {
-        if(ins->name() == "@return")
-        {
-            assert(ins->inputs().size() == 1);
-            return ins->inputs().front()->get_shape();
-        }
-        return ins->get_shape();
     }
 
     void parse(const module& m)
@@ -482,11 +402,10 @@ struct mlir_program
         {
             if(ins->name() == "@param")
                 continue;
-            auto name = get_name(ins);
+            auto name = "migraphx." + ins->name();
             auto ops  = create_operation_state(name);
-            ops.add_attribute_value(get_operator_value(ins->get_operator()));
-            if(ins->name() != "@return")
-                ops.add_results({get_shape(ins)});
+            ops.add_attribute_value(ins->get_operator().to_value());
+            ops.add_results({ins->get_shape()});
 
             std::vector<MlirValue> inputs;
             transform(
@@ -494,55 +413,9 @@ struct mlir_program
             ops.add_operands(inputs);
 
             auto outputs = insert(fbody, std::move(ops));
-            if(ins->name() != "@return")
-            {
-                assert(outputs.size() == 1);
-                ins_map[ins] = outputs.front();
-            }
+            assert(outputs.size() == 1);
+            ins_map[ins] = outputs.front();
         }
-    }
-
-    code_object_op compile() MIGRAPHX_TIDY_CONST
-    {
-        mlir_pass_manager pm{mlirPassManagerCreate(ctx.get())};
-        // 1st pipeline to call
-        mlirMIGraphXAddHighLevelPipeline(pm.get());
-        // 2nd pipeline to call
-        std::string tname = get_device_name();
-        // HACK: Since MLIR can't handle the full target name
-        auto hacked_tname = tname.substr(0, tname.find(':'));
-        if(tname.size() != hacked_tname.size())
-            std::cout
-                << "*************** WARNING: MLIR may not compile the correct target features for: "
-                << tname << std::endl;
-        mlirMIGraphXAddBackendPipeline(pm.get(), hacked_tname.c_str(), "amdgcn-amd-amdhsa", "");
-        mlirPassManagerRun(pm.get(), mmodule.get());
-
-        code_object_op op{};
-        op.symbol_name                = "main";
-        op.code_object                = get_binary();
-        std::tie(op.global, op.local) = get_launch_params();
-        return op;
-    }
-
-    std::pair<std::size_t, std::size_t> get_launch_params() const
-    {
-        uint32_t attrs[2];
-        // returns block and grid sizes
-        mlirGetKernelAttrs(mmodule.get(), attrs);
-        std::size_t local  = attrs[0];
-        std::size_t global = local * attrs[1];
-        return {global, local};
-    }
-
-    value::binary get_binary() const
-    {
-        int size = 0;
-        mlirGetBinary(mmodule.get(), &size, nullptr);
-        value::binary result(size);
-        if(mlirGetBinary(mmodule.get(), &size, reinterpret_cast<char*>(result.data())))
-            return result;
-        MIGRAPHX_THROW("Failed to compile mlir program");
     }
 
     mlir_context ctx;
@@ -558,89 +431,6 @@ std::string dump_mlir(const module& m)
     auto mod_op = mlirModuleGetOperation(mp.mmodule.get());
     return mlir_print(&mlirOperationPrint, mod_op);
 }
-
-code_object_op compile_mlir(const context&, const module& m)
-{
-    const bool trace = enabled(MIGRAPHX_TRACE_MLIR{});
-    if(trace)
-        std::cout << m << std::endl;
-    mlir_program mp;
-    mp.parse(m);
-    auto mod_op = mlirModuleGetOperation(mp.mmodule.get());
-    if(trace)
-        std::cout << mlir_print(&mlirOperationPrint, mod_op) << std::endl;
-    auto co   = mp.compile();
-    co.output = m.get_output_shapes().front();
-    return co;
-}
-
-instruction_ref insert_mlir(module& m,
-                            instruction_ref ins,
-                            code_object_op co,
-                            const std::vector<instruction_ref>& inputs)
-{
-    std::vector<instruction_ref> refs;
-    refs.reserve(inputs.size() * 15);
-
-    std::unordered_map<uint64_t, instruction_ref> literal_map{};
-    auto get_literal = [&](uint64_t value) {
-        auto fi = literal_map.find(value);
-        if(fi != literal_map.end())
-            return fi->second;
-        auto lit = m.add_literal(value);
-        literal_map.emplace(value, lit);
-        return lit;
-    };
-
-    std::size_t last = 0;
-    for(auto input : inputs)
-    {
-        const size_t offset = 0;
-        auto s              = input->get_shape();
-        last                = refs.size();
-        refs.push_back(input);
-        refs.push_back(input);
-        refs.push_back(get_literal(offset)); // offset
-
-        // dim sizes
-        std::transform(s.lens().begin(),
-                       s.lens().end(),
-                       std::back_inserter(refs),
-                       [&](const auto& lval) { return get_literal(lval); });
-        // refs.push_back(get_literal(1)); // G
-
-        // dim strides
-        std::transform(s.strides().begin(),
-                       s.strides().end(),
-                       std::back_inserter(refs),
-                       [&](const auto& lval) { return get_literal(lval); });
-        // refs.push_back(get_literal(1)); // G
-    }
-    co.expected_inputs = to_shapes(refs);
-    co.output_arg      = last;
-    return m.insert_instruction(ins, co, refs);
-}
-
-#else
-
-std::string dump_mlir(const module&) { return {}; }
-
-code_object_op compile_mlir(const context&, const module&) { return {}; }
-
-template <class T>
-void use(T&)
-{
-}
-
-instruction_ref
-// cppcheck-suppress funcArgNamesDifferent
-insert_mlir(module& m, instruction_ref, code_object_op co, const std::vector<instruction_ref>&)
-{
-    use(co);
-    return m.end();
-}
-
-#endif
 
 } // namespace gpu
 } // namespace MIGRAPHX_INLINE_NS
